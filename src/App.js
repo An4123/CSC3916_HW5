4095e284b48a3e2b8bb416c7f1879ae519310b9d
An4123 <anvo2016026@gmail.com>
2021-03-11 19:10:06 -0700

    asdf

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {        // this is <App/ >
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Deez <code>src/App.js</code> and save to reload.
+          Deez nutz <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
