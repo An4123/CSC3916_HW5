12452db20ecdd7ec247fdfa0653157792a1c571a
An4123 <anvo2016026@gmail.com>
2021-03-11 19:02:00 -0700

    test

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+ReactDOM.render(       // React Document Object Model. Render 
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root')       // get the root element and replace it into <App/ >
 );
 
 // If you want to start measuring performance in your app, pass a function
